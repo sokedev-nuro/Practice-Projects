9ccc62fab67bfc35c10db1d7c96274788674a66c
sokedev-nuro <nural.khalafov@gmail.com>
2026-03-02 10:58:19 +0100

    02.03.26 update

--- generics/scripts/PersonManager.cs
+++ generics/scripts/PersonManager.cs
@@ -1,6 +1,19 @@
 using Godot;
-using System;
 
 public partial class PersonManager : Node
 {
+    // public override void _Ready()
+    // {
+    //     Person<int> bob = new Person<int>(123, "Bob");
+    //     Person<string> tom = new Person<string>("id3", "Tom");
+
+    //     GD.Print($"Person Id: {bob.Id}, Person Name: {bob.Name}");
+    //     GD.Print($"Person Id: {tom.Id}, Person Name: {tom.Name}");
+    // }
+
+    public override void _Ready()
+    {
+        for (int i = 1, j = 1; i < 10; i++, j++)
+            GD.Print($"{i * j}");
+    }
 }
